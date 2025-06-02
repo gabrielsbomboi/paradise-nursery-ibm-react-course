2d16196016db70b1658bb00b26ea9033509d3035
Gabriel <gabrielsbomboi@gmail.com>
2025-06-02 17:00:57 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/paradise-nursery/',
+  base: '/paradise-nursery-ibm-react-course/',
   plugins: [react()],
 })
